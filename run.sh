52db807e932425bf00e498c617e3340f3e6670d9
Marvin Blum <mb@skalar-design.de>
2022-07-04 15:21:40 +0200

    Oogway

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Running http-server..."
-gnome-terminal -- /bin/sh -c 'http-server; sleep infinity'
-echo "Running node-sass..."
-gnome-terminal -- /bin/sh -c 'node-sass --watch static/style/main.scss static/dist/main.css'

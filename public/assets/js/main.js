05cd0326bea293a470f2a00272d0440e44c16e48
Marvin Blum <marvin.blum@emvi.com>
2025-08-27 12:00:55 +0200

    Slideshow, AOS, content improvements.

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,12 @@
-console.log("Hi, we're Emvi!");
+(function() {
+    document.addEventListener("DOMContentLoaded", () => {
+        document.querySelectorAll(".projects-slides").forEach(container => {
+            new Glide(container, {
+                animationDuration: 800,
+                hoverpause: false,
+                keyboard: true,
+                gap: 0
+            }).mount();
+        });
+    });
+})();
